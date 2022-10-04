2e4629e530f64c8d89bbf42624ad878a4d9e2e8f
NestorRV <nestor1453@alumnos.udg.mx>
2022-10-03 19:54:20 -0500

    video 55

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,13 +6,32 @@
         <title>DevStagram - @yield('titulo')</title>
         @vite('resources/css/app.css')
     </head>
-    <body>
+    <body class = "bg-gray-100">
+        <header class = "p-5 border-b bg-white shadow">
+            <div class = "container mx-auto flex justify-between items-center">
+                <h1 class = "text-3xl front-black">
+                    DevStagram
+                </h1>
 
-        <h1 class="text-4xl front-extrabold">@yield('titulo')</h1>
-        
-        <hr>
+                <nav class="flex gap-2 items-center">
+                    <a class="front-bold uppercase text-gray-600 text-sm" href="#">Login</a>
+                    <a class="front-bold uppercase text-gray-600 text-sm" href="/crear-cuenta">
+                        Crear Cuenta
+                    </a>
+                </nav>
 
-        @yield('contenido')
+            </div>
+        </header>
 
+        <main class="container mx-auto mt-10">
+            <h2 class="font-black text-center text-3xl mb-10">
+                @yield('titulo')
+            </h2>    
+            @yield('contenido')
+        </main>
+
+        <footer class="text-center p-5 text-gray-500 font-bold uppercase">
+            DevStagram - Todos los derechos reservados {{now()->year}}
+        </footer>
     </body>
 </html>
